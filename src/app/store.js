29150a14ee55cb0f0ac6bc11aa4230281306c72f
Bryan Vicente <bavicente98@gmail.com>
2022-06-02 17:46:25 -0500

    cleaned counterSlice from redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import themeSlice from '../features/theme/themeSlice';
+import todosSlice from '../features/todosList/todosSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     theme: themeSlice,
+    todos: todosSlice
   },
 });
